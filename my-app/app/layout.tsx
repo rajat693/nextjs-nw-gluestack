f8e93f97b0f23fdd89bd4cc640b384dbc2adc615
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-05-06 15:52:44 +0530

    feat: setup done

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import StyledJsxRegistry from "./registry";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className} style={{ display: "flex" }}>
+        <StyledJsxRegistry>
+          <GluestackUIProvider mode="light">{children}</GluestackUIProvider>
+        </StyledJsxRegistry>
+      </body>
+    </html>
+  );
+}
